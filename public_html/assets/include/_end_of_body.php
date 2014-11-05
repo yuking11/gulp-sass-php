a98311f973e83407a8c45874b18af59402b2338f
y.saito <y.saito@coobal.co.jp>
2014-11-05 15:46:04 +0900

    change jQuery version

--- public_html/assets/include/_end_of_body.php
+++ public_html/assets/include/_end_of_body.php
@@ -1,8 +1,10 @@
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo HTTP; ?>/assets/javascripts/libs/jquery-1.11.1.min.js"><\/script>')</script>
 <?php if($regacy_IE): ?>
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo HTTP; ?>/assets/javascripts/libs/jquery-1.11.1.min.js"><\/script>')</script>
 <!--[if lt IE 9]><script type="text/javascript" src="<?php echo HTTP; ?>/assets/javascripts/libs/selectivizr-min.js"></script><![endif]-->
+<?php else: ?>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo HTTP; ?>/assets/javascripts/libs/jquery-2.1.1.min.js"><\/script>')</script>
 <?php endif; ?>
 <script src="<?php echo HTTP; ?>/assets/javascripts/libs/css_browser_selector.min.js"></script>
 <script src="<?php echo HTTP; ?>/assets/javascripts/libs/nivo-lightbox.min.js"></script>
