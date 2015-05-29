2f5df9c04f83f7a1ffd0570be466349ea71a72a2
yuking <y.saito16@gmail.com>
2015-05-29 18:27:41 +0900

    update site_config.php

--- public_html/assets/include/site_config.php
+++ public_html/assets/include/site_config.php
@@ -1,17 +1,56 @@
 <?php
 
-	define("DOMAIN", $_SERVER["SERVER_NAME"]);
-	define("PRODUCTION", 'www.example.com');
-	define("DEVELOP", 'example-test.com');
+  /*
+   * URL設定
+  */
+  $base_url = isset($_SERVER['HTTPS']) &&
+              strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+  $base_url .= '://' . $_SERVER['HTTP_HOST'];
 
-	if(DOMAIN === PRODUCTION){
-		define("HTTPPATH", "http://www.example.com");
-	} else if(DOMAIN === DEVELOP){
-		define("HTTPPATH", "http://example-test.com");
-	}
+  $host = $_SERVER["HTTP_HOST"];
+  $host_productions = array('example.com', 'www.exaplem.com');
+  $host_develop = 'example.com.s-rep.net';
+  $host_local = 'local.example.com';
 
-	define("HTTP", htmlspecialchars(HTTPPATH, ENT_QUOTES));
+  if( in_array($host, $host_productions) ) {
+    //define("HTTP", 'http://www.example.com');
+  } else if($host === $host_develop){
+    //define("HTTP", 'http://example.com.s-rep.net');
+  } else if($host === $host_local){
+    //define("HTTP", 'http://local.example.com');
+  }
+  define("HTTP", $base_url);
 
+  $current_url = $base_url . $_SERVER['REQUEST_URI'];
+  define("CURRENTURL", htmlspecialchars($current_url, ENT_QUOTES));
+
+  /*
+   * 関数
+  */
+  function home_url( $atts ) {
+    if( isset($atts) ){
+      $url = HTTP . $atts;
+    } else {
+      $url = HTTP;
+    }
+    return $url;
+  }
+  function get_template_directory_uri() {
+    if( isset($atts) ){
+      $url = HTTP . '/assets' . $atts;
+    } else {
+      $url = HTTP . '/assets';
+    }
+    return $url;
+  }
+  function esc_url( $atts ) {
+    $url = htmlspecialchars($atts, ENT_QUOTES);
+    return $url;
+  }
+
+  /*
+   * サイト設定
+  */
 	// regacy support
 	$regacy_IE = false;
 
