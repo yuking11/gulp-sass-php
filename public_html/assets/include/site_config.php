5caed711d1b0c9be1bcee44919ba73fb5fc13ebd
yuking <y.saito16@gmail.com>
2015-08-28 12:55:48 +0900

    add is_mobile() is_regacy_IE()

--- public_html/assets/include/site_config.php
+++ public_html/assets/include/site_config.php
@@ -10,7 +10,7 @@
   $host = $_SERVER["HTTP_HOST"];
   $host_productions = array('example.com', 'www.exaplem.com');
   $host_develop = 'example.com.s-rep.net';
-  $host_local = 'local.example.com';
+  $host_local  = 'localhost'.strstr($host, ':');
 
   if( in_array($host, $host_productions) ) {
     //define("HTTP", 'http://www.example.com');
@@ -47,6 +47,35 @@
     $url = htmlspecialchars($atts, ENT_QUOTES);
     return $url;
   }
+  // モバイル判定
+  function is_mobile() {
+    $ua = array(
+      'iPhone',         // Apple iPhone
+      'iPod',           // Apple iPod touch
+      'iPad',           // Apple iPad
+      'Android',        // 1.5+ Android
+      'dream',          // Pre 1.5 Android
+      'CUPCAKE',        // 1.5+ Android
+      'blackberry9500', // Storm
+      'blackberry9530', // Storm
+      'blackberry9520', // Storm v2
+      'blackberry9550', // Storm v2
+      'blackberry9800', // Torch
+      'webOS',          // Palm Pre Experimental
+      'incognito',      // Other iPhone browser
+      'webmate'         // Other iPhone browser
+    );
+    $pattern = '/'.implode('|', $ua).'/i';
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    return preg_match($pattern, $agent);
+  }
+  // レガシーIE判定
+  function is_regacy_IE() {
+    $regacy_ies = array('MSIE 7.0', 'MSIE 8.0');
+    $pattern = '/'.implode('|', $regacy_ies).'/i';
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    return preg_match($pattern, $agent);
+  }
 
   /*
    * サイト設定
@@ -56,11 +85,9 @@
   $regacy_IE = false;
 
 	// body class
-  if( isset($slug_child) ){
-    $body_class = $slug . '__' . $slug_child;
-  } else {
-    $body_class = $slug;
-  }
+  $body_class = isset($slug_child) ? $slug . '__' . $slug_child : $slug;
+
+  // PAGE_NAME
   define('PAGE_NAME', htmlspecialchars($body_class, ENT_QUOTES));
 
 ?>
