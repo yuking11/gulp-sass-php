1a9c0865e612b67577c69c86496b9d11e59f2bac
yuking <y.saito16@gmail.com>
2016-12-17 18:19:35 +0900

    php endif 消し忘れ

--- public_html/assets/include/_end_of_body.php
+++ public_html/assets/include/_end_of_body.php
@@ -1,6 +1,5 @@
 <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo HTTP; ?>/assets/javascripts/vendor/jquery-3.1.1.min.js"><\/script>')</script>
-<?php endif; ?>
 <script src="<?php echo HTTP; ?>/assets/javascripts/vendor/css_browser_selector.min.js"></script>
 <script src="<?php echo HTTP; ?>/assets/javascripts/vendor/nivo-lightbox.min.js"></script>
 <script src="<?php echo HTTP; ?>/assets/javascripts/application.min.js"></script>
